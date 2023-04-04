9ae1ff4733d1d0047eb70eb6daedb146a85b2c1f
LiquidRadish <nickchristovich@gmail.com>
2023-04-04 11:57:46 -0400

    sawblades now kill player

--- player.gd
+++ player.gd
@@ -4,24 +4,39 @@ var speed = 10.0
 const MAX_SPEED = 100
 var jumpVelocity = -300.0
 
+const START_POS = Vector2(240, 450)
+
+signal hit
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 '''
-TODO:
+TTODO:
 	- Dial in movement, jump height
 		- Current movement is sorta squishy (maybe we like it that way?)
 		- Air movement is weird at high speeds
+		
+	- Collision with sawblade is weird
 
 '''
 
+# can be used to reset or change location 
+func start(pos):
+	position = pos
+	velocity.x = 0
+	velocity.y = 0
+	show()
+	
+
+# Called every frame
 func _process(delta: float) -> void:
 	update_animation()
 	
-
-func update_animation():
 	
+# Updates the character to have the correct sprite based on direction and input
+func update_animation():
+	# Face the correct direction
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
@@ -34,6 +49,8 @@ func update_animation():
 	else:
 		$AnimatedSprite2D.play("jump_right")
 
+
+# Handles movement input and changes in velocity
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -55,4 +72,13 @@ func _physics_process(delta):
 	if not Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		velocity.x = move_toward(velocity.x, 0, speed * 2)
 
+	# Actually update position
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		# TODO : there has to be a better way to detect collision with specific object...
+		if collision.get_collider().to_string().to_lower().contains('sawblade'):
+			start(START_POS)
+
+
