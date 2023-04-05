4727ce2b7458baa296082088be031eee199132b7
Caleb <wolfertamer@gmail.com>
2023-04-04 21:57:46 -0700

    Added Camera and Level loading changes

---
+++ NextLevel.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+var entered = false
+
+func _on_hitbox_body_entered(body):
+	entered = true
+		
+
+
+func _on_hitbox_body_exited(body):
+	entered = false
+	
+func _process(delta):
+	if entered:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://ControlRoom.tscn")
