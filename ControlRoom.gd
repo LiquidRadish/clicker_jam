b30f3c1c54e74b56f1a84d6e1e99221d6dc2741a
Caleb <wolfertamer@gmail.com>
2023-04-07 13:39:16 -0700

    Two random upgrades appear in control room when loaded.

--- ControlRoom.gd
+++ ControlRoom.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
+var rng = RandomNumberGenerator.new()
+
 func _ready():
-	var newshop = preload("res://shop_object.tscn").instantiate()
-	newshop.interface = $CanvasLayer/Interface
-	newshop.position = Vector2(130,165)
-	newshop.color = Color(0,1,0)
-	newshop.nameOfUpgrade = "Move Speed"
-	newshop.var_to_change = 1
-	self.add_child(newshop)
+	# Istantiates two upgrade nodes and sets their position
+	var items = [preload("res://shop_object.tscn").instantiate(), preload("res://shop_object.tscn").instantiate()]
+	items[0].position = Vector2(100,170)
+	items[1].position = Vector2(140,170)
+	
+	# Randomly chooses the upgrade's type and adds it to the scene
+	for item in items:
+		item.interface = $CanvasLayer/Interface
+		match rng.randi_range(0,1):
+			0:
+				item.color = Color(0,0,1)
+				item.nameOfUpgrade = "Jump Height"
+				item.var_to_change = 0
+			1:
+				item.color = Color(0,1,0)
+				item.nameOfUpgrade = "Move Speed"
+				item.var_to_change = 1
+		self.add_child(item)
--- shop_object.gd
+++ shop_object.gd
@@ -19,13 +19,14 @@ var entered = false
 func _ready():
 	modulate = color
 
+# When player collides with the object show text display at bottom of the screen
 func _on_hitbox_body_entered(body):
 	entered = true
 	var label:Label = interface.get_child(1)
 	if label != null:
 		label.text = "Press Enter to Buy: \n" + nameOfUpgrade + " for " + str(cost) + " clicks"
 
-
+# When player leaves collision get rid of text
 func _on_hitbox_body_exited(body):
 	entered = false
 	var label:Label = interface.get_child(1)
@@ -37,20 +38,24 @@ func _process(delta):
 	if destroying == 0 && entered:
 		var label:Label = interface.get_child(1)
 		if label != null && Input.is_action_just_pressed("ui_accept"):
+			
 			if GlobalVariables.click_count >= cost:
+				#Set texture and animation to coin pickup
 				texture = preload("res://assets/images/CoinPickup.png")
 				state_machine.travel("Coin Pickup")
 				GlobalVariables.click_count-= cost
+				
 				match var_to_change:
 					0:
 						GlobalVariables.jump_speed += 20
 					1:
 						GlobalVariables.move_speed += 20
+						
 				label.text = ""
 				destroying = 1
 			else:
 				label.text = "Too expensive"
 
-
+#Destroy object once coin pickup animation is done
 func _on_animation_tree_animation_finished(anim_name):
 	if anim_name == "Coin Pickup": queue_free()
